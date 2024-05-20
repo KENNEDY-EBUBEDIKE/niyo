20be0de59d8322549e929f10c02d3c6a1d0115d6
kennedy <kennedyeziechina@gmail.com>
2024-05-20 19:42:17 +0100

    Updated docker compose and entrypoint files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+# Create migrations
+echo "Running migrations..."
+python3 manage.py makemigrations
+
+# Run database migrations
+echo "Running migrations..."
+python3 manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
+
 # Start Gunicorn for WSGI server
 echo "Starting Gunicorn..."
 gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application &
